9f5576857150163f7d44e65b9e0833618f772128
Kendra Kwoka <kendrakwoka@gmail.com>
2020-02-13 18:49:10 -0600

    changed questions/answers

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,31 +1,93 @@
+// var questions = [
+//     { 
+//     title: "Commonly used data types DO NOT include:",
+//     choices: ["strings", "booleans", "alerts", "numbers"],
+//     answer: "alerts"
+//     },
+
+//     {
+//     title: "The condition in an if / else statement is enclosed within ____.",
+//     choices: ["quotes", "curly brackets", "square brackets", "parentheses"],
+//     answer: "parentheses"
+//     },
+
+//     {
+//     title: "Which of the following is NOT a data type?",
+//     choices: ["number", "string", "boolean", "NaN"],
+//     answer: "NaN"
+//     },
+
+//     {
+//     title: "Which of the following is NOT a loop type?",
+//     choices: ["circuit", "while", "for", "do-while"],
+//     answer: "circuit"
+//     },
+    
+//     {
+//     title: "What of the following is NOT a pop-up?",
+//     choices: ["alert", "display", "confirm", "prompt"],
+//     answer: "display"
+//     },
+// ];
+
 var questions = [
     { 
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "How many rings are on the Olympic flag:",
+    choices: ["none", "4", "5", "7"],
+    answer: "5"
     },
 
     {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "square brackets", "parentheses"],
-    answer: "parentheses"
+    title: "How did Spider-Man get his powers?",
+    choices: ["Military experiment gone awry", "He was born with them", "Nuclear fallout", "Bitten by a radioactive spider"],
+    answer: "Bitten by a radioactive spider"
     },
 
     {
-    title: "Which of the following is NOT a data type?",
-    choices: ["number", "string", "boolean", "NaN"],
-    answer: "NaN"
+    title: "In darts, what's the most points you can score with a single throw?",
+    choices: ["20", "50", "60", "100"],
+    answer: "60"
     },
 
     {
-    title: "Which of the following is NOT a loop type?",
-    choices: ["circuit", "while", "for", "do-while"],
-    answer: "circuit"
+    title: "Which of the following animals does NOT appear in the Chinese zodiac",
+    choices: ["Bear", "Rabbit", "Dragon", "Dog"],
+    answer: "Bear"
     },
     
     {
-    title: "What of the following is NOT a pop-up?",
-    choices: ["alert", "display", "confirm", "prompt"],
-    answer: "display"
+    title: "Who are known as Brahmins?",
+    choices: ["Surfers in California", "Napali soldiers in the British Army", "Members of India's highest caste", "It's a totally made up word"],
+    answer: "Members of India's highest caste"
+    },
+
+    {
+    title: "How many holes are in a bowling ball?",
+    choices: ["1", "2", "3", "4"],
+    answer: "3"
+    },
+
+    {
+    title: "What is a pomelo?",
+    choices: ["A large citrus fruit", "Something that cheerleaders hold", "A breed of dog", "A type of punching bag"],
+    answer: "A large citrus fruit"
+    },
+
+    {
+    title: "Who killed Greedo?",
+    choices: ["Hannibal Lecter", "Han Solo", "Bugs Bunny", "Hercules"],
+    answer: "Han Solo"
+    },
+
+    {
+    title: "Are giant pandas a type of bear?",
+    choices: ["Yes", "No"],
+    answer: "Yes"
+    },
+
+    {
+    title: "How many points is the letter X worth in Scrabble?",
+    choices: ["0", "10", "8", "11"],
+    answer: "8"
     },
 ];
